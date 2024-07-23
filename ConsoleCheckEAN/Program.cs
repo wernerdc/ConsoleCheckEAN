1d490e2e63607767f5a3d6293e593fb9864c47e1
wernerdc <manuel.werner3@gmx.de>
2024-07-24 00:24:23 +0200

    final version 1.0 (string format)

--- ConsoleCheckEAN/Program.cs
+++ ConsoleCheckEAN/Program.cs
@@ -10,16 +10,36 @@
                 Console.Clear();
                 Console.WriteLine("ConsoleCheckEAN \n");
 
-                long ean;
-                try {
-                    Console.Write("Bitte geben Sie eine EAN ein: ");
-                    ean = Convert.ToInt64(Console.ReadLine());
-                } catch {
-
-                    throw;
+                long ean = 0;
+                while (ean <= 0) {
+                    try {
+                        Console.Write("Bitte geben Sie eine EAN ein: ");
+                        //ean = Convert.ToInt64(Console.ReadLine());
+                        ean = long.Parse(Console.ReadLine());
+                        if (ean <= 0 || ean.ToString().Length > 13) {
+                            ShowInputErrorMessage();
+                        }
+                    } catch {
+                        ShowInputErrorMessage();
+                    }
+                }
+                
+                string eanString13 = string.Format("{0:D13}", ean);
+                int sum = 0;
+                for (int i = 0; i < eanString13.Length; i++) {
+                    if (i % 2 == 0) {
+                        sum += int.Parse(eanString13[i].ToString());
+                    } else {
+                        sum += int.Parse(eanString13[i].ToString()) * 3;
+                    }
                 }
 
-                Console.Write("\n\nProgramm beenden (e)? ");
+                string isValid = (sum % 10 == 0) ? "gültig" : "ungültig";
+                Console.WriteLine("Summe: {0}\n", sum);
+                Console.WriteLine("Die EAN {0:0 000 000 000 000} ist {1}!\n", ean, isValid);
+
+
+                Console.Write("\nProgramm beenden (e)? ");
                 try {
                     string? exitApp = Console.ReadLine();
                     if (exitApp.ToUpper() == "E") {
@@ -30,6 +50,13 @@
                 }
             }
 
+        }
+
+        private static void ShowInputErrorMessage() {
+
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.WriteLine("\nUngültige Eingabe: Nur positive Ganzzahlen sind erlaubt\n");
+            Console.ForegroundColor = ConsoleColor.Gray;
         }
     }
 }
