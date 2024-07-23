de9f034a935059054f17290f97e7f5acffedcebe
wernerdc <manuel.werner3@gmx.de>
2024-07-23 22:17:21 +0200

    Projektdateien hinzufügen.

---
+++ ConsoleCheckEAN/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace ConsoleCheckEAN {
+    internal class Program {
+        static void Main(string[] args) {
+
+            bool appRunning = true;
+            while (appRunning) {
+
+                Console.BackgroundColor = ConsoleColor.Blue;
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Console.Clear();
+                Console.WriteLine("ConsoleCheckEAN \n");
+
+                long ean;
+                try {
+                    Console.Write("Bitte geben Sie eine EAN ein: ");
+                    ean = Convert.ToInt64(Console.ReadLine());
+                } catch {
+
+                    throw;
+                }
+
+                Console.Write("\n\nProgramm beenden (e)? ");
+                try {
+                    string? exitApp = Console.ReadLine();
+                    if (exitApp.ToUpper() == "E") {
+                        appRunning = false;
+                    }
+                } catch {
+                    // no error message, just keep going and repeat the app
+                }
+            }
+
+        }
+    }
+}
